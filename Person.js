4b1c7e26006daf7af4c39bb3c607786d4be4cf6e
Alejandro Baez <abaez2623@gmail.com>
2023-07-25 12:25:22 -0400

    add day 1 person class and main function

---
+++ Person.js
@@ -0,0 +1,33 @@
+//class is the blueprint and it creates an object
+//constructor creates the object, takes in parameters
+class Person{
+    constructor(name, type , location){
+        this.name = name;
+        this.type = type;
+        this.location = location || [0,0]
+    }
+
+    introduce(){
+        return `Hello my name is ${this.name}`
+    }
+
+    updateX(newX){
+        this.location[0] = newX
+
+    }
+
+    updateY(newY){
+        this.location[1] = newY
+
+    }
+    
+
+}
+
+//used to show exports
+function testFunction(){
+    return "Hello World"
+}
+
+
+module.exports = {Person, testFunction};
